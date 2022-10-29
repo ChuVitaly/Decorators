b3b0c5f467039edc74249a274755db23daa5178a
Виталий Чухляев <vitaliy69@mail.ru>
2022-10-29 22:28:57 +0300

    Task finalized

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import time
 
+path = "data.txt"
+def parametrized_decor(path):
+    def test_func(func):
+       def wrapper(*args, **kwargs):
+          with open(path, "w", encoding="utf-8") as f:
+              res = func(*args, **kwargs)
+              f.write(res)
+              return res
 
-def test_func(func):
-   def wrapper(*args, **kwargs):
-      with open("data.txt", "w", encoding="utf-8") as f:
-          res = func(*args, **kwargs)
-          f.write(res)
-          return res
+       return wrapper
+    return test_func
 
-   return wrapper
-
-@test_func
+@parametrized_decor(path)
 def sum_fun(a, b, seconds=None):
   x = a + b
   local_time = time.ctime(seconds)
   return f"{local_time}, Сумма двух чисел {a} и {b} равна: {x}"
 
 
-res = sum_fun(250, 350)
 
 
 
 if __name__ == '__main__':
+    res = sum_fun(250, 350)
     print(res)
